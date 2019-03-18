8493de4a11f209afbadbcdf350298191372cbaf1
Mantas <jmlmantas@gmail.com>
2019-03-18 20:36:53 +0200

    Chunk generation

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour{
+
+    public GameObject DirtTile;
+    public GameObject StoneTile;
+    public GameObject GrassTile;
+
+    public int width;
+    public float heightMult;
+    public int heightAddition;
+
+    public float smoothness;
+
+    [HideInInspector]
+    public float seed;
+
+    void Start(){
+        Generate();
+    }
+
+    public void Generate(){
+        for(int i = 0; i < width; i++){
+            int h = Mathf.RoundToInt(Mathf.PerlinNoise(seed, (i + transform.position.x) / smoothness) * heightMult) + heightAddition;
+            for(int j = 0; j < h; j++){
+                GameObject selectedTile;
+                if(j < h - 4){
+                    selectedTile = StoneTile;
+                }else if(j < h - 1){
+                    selectedTile = DirtTile;
+                }else{
+                    selectedTile = GrassTile;
+                }
+                GameObject newTile = Instantiate(selectedTile, Vector3.zero, Quaternion.identity) as GameObject;
+                newTile.transform.parent = this.gameObject.transform;
+                newTile.transform.localPosition = new Vector3(i, j);
+            }
+        }
+    }
+   
+}
---
+++ Assets/Scripts/GenerateChunks.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateChunks : MonoBehaviour{
+
+    public GameObject chunk;
+    int chunkWidth;
+    public int chunkNum;
+    float seed;
+
+    void Start(){
+        chunkWidth = chunk.GetComponent<Chunk>().width;
+        seed = Random.Range(-100000f, 100000f);
+        Generate();
+    }
+
+    public void Generate(){
+        int lastX = -chunkWidth;
+        for(int i = 0; i < chunkNum; i++){
+            GameObject newChunk = Instantiate(chunk, new Vector3(lastX + chunkWidth, 0f), Quaternion.identity) as GameObject;
+            newChunk.GetComponent<Chunk>().seed = seed;
+            lastX += chunkWidth;
+        }
+    }
+}
